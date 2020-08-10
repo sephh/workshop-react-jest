1aaab4be24f626500e380230b0bcb8671de80680
greg <greg.almeidasilva@gmail.com>
2020-08-10 14:53:55 -0300

    solution user-events

--- src/components/PokemonCard/__test__/PokemonCard.test.js
+++ src/components/PokemonCard/__test__/PokemonCard.test.js
@@ -1,1 +1,35 @@
-// TODO testar component
+import React from "react";
+import {render} from "@testing-library/react";
+
+import PokemonCard from "../PokemonCard";
+import {cardBuilder} from "../../../__mocks__/card-builder";
+
+describe('PokemonCard', () => {
+    let card;
+
+    beforeEach(() => {
+        card = cardBuilder();
+    });
+
+    test('should render with default props', ()=>{
+        const {container, getByAltText} = render(<PokemonCard {...card}/>);
+        const imageAlt = `${card.id}-${card.name}`;
+
+        const image = getByAltText(imageAlt);
+
+        expect(container).toBeInTheDocument();
+        expect(image.src).toBe(card.imageUrl);
+        expect(image.alt).toBe(imageAlt);
+    });
+
+    test('should emit event onClick', () => {
+        const onClick = jest.fn();
+        const {getByAltText} = render(<PokemonCard {...card} onClick={onClick}/>);
+        const imageAlt = `${card.id}-${card.name}`;
+
+        const image = getByAltText(imageAlt);
+        image.click();
+
+        expect(onClick).toHaveBeenCalledTimes(1);
+    });
+});
--- src/components/SearchBar/__test__/SearchBar.test.js
+++ src/components/SearchBar/__test__/SearchBar.test.js
@@ -1,2 +1,47 @@
-// TODO testar component
+import React from "react";
+import {fireEvent, render} from "@testing-library/react";
+import SearchBar from "../SearchBar";
 
+const setup = (props = {}) => {
+    const renderResult = render(<SearchBar {...props}/>);
+
+    return {
+        input: renderResult.getByPlaceholderText('Pesquise...'),
+        button: renderResult.getByText('Botão'),
+        defaultInputDelay: 200,
+        ...renderResult
+    };
+};
+
+jest.useFakeTimers();
+
+describe('SearchBar', () => {
+    test('should render with default props', () => {
+        const {container, input, button} = setup();
+
+        expect(container).toBeInTheDocument();
+        expect(input).toBeInTheDocument();
+        expect(button).toBeInTheDocument();
+    });
+
+    test('input should emit onChange event', () => {
+        const onChange = jest.fn();
+        const {input, defaultInputDelay} = setup({onChange});
+
+        fireEvent.change(input, {target: {value: 'Picles'}});
+
+        jest.runTimersToTime(defaultInputDelay);
+
+        expect(onChange).toHaveBeenCalled();
+        expect(onChange).toHaveBeenCalledWith({target: input});
+    });
+
+    test('button should emit onButtonClick event', () => {
+        const onButtonClick = jest.fn();
+        const {button} = setup({onButtonClick});
+
+        button.click();
+
+        expect(onButtonClick).toHaveBeenCalledTimes(1);
+    });
+});
